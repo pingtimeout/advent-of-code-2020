6abb9ea682a50d53471fe6cd5d780d9adbad915d
Pierre Laporte <pierre@pingtimeout.fr>
2020-12-21 22:38:31 +0100

    Add Day03

---
+++ src/main/scala/fr/pingtimeout/adventofcode2020/Day03.scala
@@ -0,0 +1,37 @@
+package fr.pingtimeout.adventofcode2020
+
+case class Day03(mode: Mode) extends AdventOfCode {
+  override val dayName: String = "day03"
+  lazy val map: IndexedSeq[IndexedSeq[Char]] = text(mode).map(_.toSeq).toIndexedSeq
+  lazy val width = map(0).size
+  lazy val height = map.size
+
+  def countEncounters(horizontalStep: Int, verticalStep: Int) = {
+    (0 until (height / verticalStep))
+      .map(iteration => (verticalStep * iteration, horizontalStep * iteration % width))
+      .map { case (a, b) => if (map(a)(b) == '#') 1 else 0 }
+      .sum
+      .toLong
+  }
+
+  override def solvePartOne() {
+    println(countEncounters(3, 1))
+  }
+
+  override def solvePartTwo(): Unit = {
+    println(countEncounters(1, 1)
+      * countEncounters(3, 1)
+      * countEncounters(5, 1)
+      * countEncounters(7, 1)
+      * countEncounters(1, 2))
+  }
+}
+
+object Day03 {
+  def main(args: Array[String]): Unit = {
+    Day03(Example).solvePartOne()
+    Day03(Real).solvePartOne()
+    Day03(Example).solvePartTwo()
+    Day03(Real).solvePartTwo()
+  }
+}
